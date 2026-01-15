6357d8d26564c8842804052dd4388ac163dbc6eb
Santiago Eliges <saneliges@gmail.com>
2026-01-15 11:44:50 -0300

    a

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from scrapper import Scrapper
+from valorDolarHoy import DolarHoyScrapper
 import json
 import pandas as pd
+
+
+dolar_scraper = DolarHoyScrapper()
+dolar_scraper.run()
+valores_dolar = dolar_scraper.get_valores()
+print(valores_dolar)
+
+df_valores_dolar = pd.DataFrame(
+    list(valores_dolar.items()),
+    columns=["tipo_dolar", "valor"]
+)
+
+df_valores_dolar["valor"] = (
+    df_valores_dolar["valor"]
+    .str.replace(".", "", regex=False)
+    .str.replace(",", ".", regex=False)
+    .astype(float)
+)
+
+df_valores_dolar.to_csv("dolar_hoy.csv", index=False)
 
 scraper = Scrapper(headless=True)
 scraper.run()
