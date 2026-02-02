b2b34a44de5e05054205fd7c1288e48541b1c61d
santieliges <165414478+santieliges@users.noreply.github.com>
2026-02-02 12:07:10 -0300

    refactoring

--- main.py
+++ main.py
@@ -58,38 +58,38 @@ synchronizer.sync_data(storage_scapped_data.get_all())
 
 
 #Cierro los datos de urls que dieron error 410
-#synchronizer.close_endend_urls()
+synchronizer.close_endend_urls()
 scraper.close()
 
-####################### Datos Venta ########################
-#Inicializo el scrapper de venta y scrappeo n paginas
-scraper = VentaScrapper(headless=True, url_base="https://www.argenprop.com/casas-o-departamentos-o-ph/venta/capital-federal/dolares-desde-20000?orden-masnuevos")
-scraper.run()
-all_data = scraper.extract_all_pages(n_pages=2)
-pd.DataFrame(all_data).to_csv("storage/data/raw/arg_venta_data.csv", index=False)
-storage_scapped_data = CSVStorage("storage/data/raw/arg_venta_data.csv")
+# ####################### Datos Venta ########################
+# #Inicializo el scrapper de venta y scrappeo n paginas
+# scraper = VentaScrapper(headless=True, url_base="https://www.argenprop.com/casas-o-departamentos-o-ph/venta/capital-federal/dolares-desde-20000?orden-masnuevos")
+# scraper.run()
+# all_data = scraper.extract_all_pages(n_pages=5)
+# pd.DataFrame(all_data).to_csv("storage/data/raw/arg_venta_data.csv", index=False)
+# storage_scapped_data = CSVStorage("storage/data/raw/arg_venta_data.csv")
 
-# Limpio los datos scrappeados
-datacleaner = DataCleaner(storage_scapped_data)
-datacleaner.clean_expensas_column()
-datacleaner.clean_antiguedad_column()
-datacleaner.clean_precio_column()
-datacleaner.clean_area_m2_column()
-datacleaner.ender_cleaning()
+# # Limpio los datos scrappeados
+# datacleaner = DataCleaner(storage_scapped_data)
+# datacleaner.clean_expensas_column()
+# datacleaner.clean_antiguedad_column()
+# datacleaner.clean_precio_column()
+# datacleaner.clean_area_m2_column()
+# datacleaner.ender_cleaning()
 
-storage_scapped_data = CSVStorage("storage/data/clean/arg_venta_data.csv")
+# storage_scapped_data = CSVStorage("storage/data/clean/arg_venta_data.csv")
 
-# Sincronizo la data scrappeada con el storage
-storage_path = "storage/data/historic/arg_venta_data.csv"
-storage = CSVStorage(storage_path)
+# # Sincronizo la data scrappeada con el storage
+# storage_path = "storage/data/historic/arg_venta_data.csv"
+# storage = CSVStorage(storage_path)
 
-# Sincronizo datos scrapeados con los datos actuales en el storage
-synchronizer = Synchronizer(storage, scraper)
-synchronizer.sync_data(storage_scapped_data.get_all())
-#Cierro los datos de urls que dieron error 410
-#synchronizer.close_endend_urls()
+# # Sincronizo datos scrapeados con los datos actuales en el storage
+# synchronizer = Synchronizer(storage, scraper)
+# synchronizer.sync_data(storage_scapped_data.get_all())
+# #Cierro los datos de urls que dieron error 410
+# synchronizer.close_endend_urls()
 
-scraper.close()
+# scraper.close()
 
 
 
